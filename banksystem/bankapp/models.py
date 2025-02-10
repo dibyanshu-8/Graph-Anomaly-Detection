e5c6dc4a0e78081bfbdd6e253ec0a1186acb7cee
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-02-11 03:43:25 +0530

    changes in models.py

--- banksystem/bankapp/models.py
+++ banksystem/bankapp/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class branch(models.Model):
     name=models.CharField(max_length=200)
+    address=models.CharField(max_length=200)
+    branch_code=models.CharField(max_length=200)
+    
+
+    
