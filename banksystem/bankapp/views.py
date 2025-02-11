e6d531af3257259f30f9f378428b9cf869980934
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-02-11 21:01:09 +0530

    changes in views.py

--- banksystem/bankapp/views.py
+++ banksystem/bankapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
 
+from django.shortcuts import render
+from django.http import Http404
+
+from rest_framework import generics,status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
