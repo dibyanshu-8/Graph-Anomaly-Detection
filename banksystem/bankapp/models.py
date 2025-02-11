46f5053269d1c2fd6ae304abe80300846f7917e6
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-02-11 20:04:08 +0530

    changes in models.py

--- banksystem/bankapp/models.py
+++ banksystem/bankapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from __future__ import unicode_literals
 
 # Create your models here.
 class branch(models.Model):
@@ -6,5 +7,7 @@ class branch(models.Model):
     address=models.CharField(max_length=200)
     branch_code=models.CharField(max_length=200)
     
-
+    class Meta:
+         verbose_name_plural = "Branches"
+        
     
